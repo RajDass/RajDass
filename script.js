28e97501574a9f5da01c4df29c05f8f92dcb6472
RajDass <rajdass6169@gmail.com>
2023-07-07 16:51:16 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,27 @@
+// window.addEventListener('scroll',
+// function(){
+//     let navbar = document.getElementById('navbarr');
+//     if(window.pageYOffset >= 0){
+//         navbar.classList.add('sticky');
+//     }
+//     else{
+//         navbar.classList.remove('sticky');
+
+//     }
+
+// })
+
+// const headerr = document.querySelector(".header-banner");
+
+//         const stickyy = headerr.offsetTop;
+
+//         console.log(stickyy);
+
+//         window.addEventListener('scroll', () =>{
+//             if(window.pageYOffset > 500){
+//                 headerr.classList.add('sticky');
+//             }else{
+//                 headerr.classList.remove('sticky');
+
+//             }
+//         })
